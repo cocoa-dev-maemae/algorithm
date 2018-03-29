6be6e1b8d9b394bac925015c38be8e1aa5535e5a
cocoa-dev-maemae <breezin_ahimusa@yahoo.co.jp>
2018-03-29 16:13:09 +0200

    fix function name

--- c/singly_linked_list.c
+++ c/singly_linked_list.c
@@ -9,7 +9,7 @@ typedef struct cell {
 } cell_t;
 
 static cell_t *alloc_list(int data);
-static void add(cell_t *header, int data);
+static void insert(cell_t *header, int data);
 static void delete(cell_t *header);
 
 cell_t *alloc_list(int data)
@@ -25,7 +25,7 @@ cell_t *alloc_list(int data)
     return new;
 }
 
-void add(cell_t *header, int data)
+void insert(cell_t *header, int data)
 {
     cell_t *next = NULL;
     cell_t *prev = header;
@@ -33,7 +33,7 @@ void add(cell_t *header, int data)
     //create cell
     next = alloc_list(data);
 
-    // add cell to the end
+    //  cell to the end
     while (prev->next != NULL) {
         prev = prev->next;
     }
@@ -69,7 +69,7 @@ int main(int argc, char* argv)
         return 1;
     }
     for (cnt = 1; cnt < 10; cnt++) {
-        add(header, cnt);
+        insert(header, cnt);
     }
     print_list(header);
     delete(header);
